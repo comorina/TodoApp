b11f1b418327e686cad98ce8c3c88adc5500fb5c
shpandey <shivampandey91198@gmail.com>
2021-12-08 12:41:23 +0530

    all okay

--- src/myComponent/TodoList.js
+++ src/myComponent/TodoList.js
@@ -2,7 +2,7 @@ import bin from'./bin.png'; // Image Import here..
 import edit from'./edit.png';
 import React from 'react';
 import './TodoList.css';
-import { Link } from 'react-router-dom';
+//import { Link } from 'react-router-dom';
 
 
 
